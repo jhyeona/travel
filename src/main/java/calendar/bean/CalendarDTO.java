3e1526e6f6e7f967499eac597feacf56e2946197
kimmin0425 <kimmin425@naver.com>
2022-06-15 20:00:46 +0900

    DTO 설정
    DTO 설정

--- src/main/java/calendar/bean/CalendarDTO.java
+++ src/main/java/calendar/bean/CalendarDTO.java
@@ -1,5 +1,12 @@
 package calendar.bean;
 
-public class CalendarDTO {
+import lombok.Data;
 
+@Data
+public class CalendarDTO {
+	private int calendar_seq;
+	private String calendar_subject;
+	private String calendar_content;
+	private String calendar_id;
+	private String calendar_pw;
 }
--- src/main/java/review/bean/ReviewDTO.java
+++ src/main/java/review/bean/ReviewDTO.java
@@ -1,5 +1,12 @@
 package review.bean;
 
-public class ReviewDTO {
+import lombok.Data;
 
+@Data
+public class ReviewDTO {
+	private int review_seq;
+	private String review_subject;
+	private String review_content;
+	private String review_id;
+	private String review_pw;
 }
